262112915f2a9dce56ed6bec435b414b3b2031a8
Khenry Perez <55653728+khenryperez@users.noreply.github.com>
2024-01-30 22:22:18 +0800

    013024 - Update v1
    -Added information for fields.
    -Updated icons, logos and previews.

---
+++ Scripts/test.js
@@ -0,0 +1,12 @@
+// function btnProjects(){
+//     alert('This button works! - Projects');
+// }
+
+// function btnCertifications(){
+//     alert('This button works! - Certification');
+// }
+
+function btnContact(){
+    // alert('This button works! - Contact');
+    window.scrollTo(0, document.body.scrollHeight);
+}
